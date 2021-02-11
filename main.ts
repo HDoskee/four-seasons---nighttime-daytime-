6e78f75f304caf475bc2e79d808ec6dd7e073ea1
HDoskee <78930884+HDoskee@users.noreply.github.com>
2021-02-11 14:31:42 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,131 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . # . .
+        # # . # #
+        # # . # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        # # # # #
+        . # # # .
+        # . # . #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # . .
+        # . # . #
+        . # # # .
+        # # # # #
+        . . # . .
+        `)
+    basic.pause(500)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . # .
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # # .
+        . . . # #
+        . . . # #
+        . . . # #
+        . . # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        . . # # #
+        . . # # #
+        . . # # #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        . # # # #
+        . # # # #
+        . # # # #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        . # # # #
+        . # # # #
+        . # # # #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        . . # # #
+        . . # # #
+        . . # # #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # # .
+        . . . # #
+        . . . # #
+        . . . # #
+        . . # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . # .
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
